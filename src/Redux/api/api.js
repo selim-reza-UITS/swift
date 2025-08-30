89c0227a531080fbaaab270c558c54e4ef73731f
oishee90 <oisheekhan90@gmail.com>
2025-08-30 11:05:37 +0600

    update env

--- src/Redux/api/api.js
+++ src/Redux/api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://10.10.13.20:8001/api/v1/",
+  baseUrl: import.meta.env.VITE_BASE_URL,
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth?.access || null;
     console.log(token);
